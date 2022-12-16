412da7018d0ef189e30f5ed2b44f3579271d2f07
abdelaziz mrabet <mrabetabdelaziz@gmail.com>
2022-12-16 22:17:47 +0100

    adding some style for mobile

--- src/components/ShoppingCard.js
+++ src/components/ShoppingCard.js
@@ -15,7 +15,7 @@ const style = {
   top: '50%',
   left: '50%',
   transform: 'translate(-50%, -50%)',
-  width: 500,
+  width: 400,
   bgcolor: 'wheat',
   border: '2px solid whitesmoke',
   boxShadow: 24,
